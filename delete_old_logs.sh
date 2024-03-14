3ebf9a0ac1c115d4b371cd5c6b3869de9126a610
Luke Marsden <lukem@met.no>
2024-03-14 15:34:33 +0100

    small updates

--- delete_old_logs.sh
+++ delete_old_logs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 config_file="config/config.yml"
-product_keep_hours=$(grep 'product_keep_hours' ${config_file} | awk '{print $2}')
-lustre_NetCDFs_path=$(grep 'lustre_NetCDFs_path' ${config_file} | awk '{print $2}')
+logs_keep_days=$(grep 'logs_keep_days' ${config_file} | awk '{print $2}')
+tmp_logs_dir=$(grep 'tmp_logs_dir' ${config_file} | awk '{print $2}')
 
-# Find all files with .nc suffix older than the defined age and delete them
-find "$lustre_NetCDFs_path" -type f -name "*.nc" -mtime +$product_keep_hours -exec rm -f {} \;
+# Find all log files older than the defined age and delete them
+find "$tmp_logs_dir" -type f -name "*.log" -mtime +$logs_keep_days -exec rm -f {} \;
